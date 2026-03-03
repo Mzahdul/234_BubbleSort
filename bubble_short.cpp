8f63b7c38fc7994131a758bcdf3b6f962779ef4f
Mzahdul <seeglas8@gmail.com>
2026-03-03 11:44:50 +0700

    mengurutkan array sampai menampilkan data pada number

--- bubble_short.cpp
+++ bubble_short.cpp
@@ -10,12 +10,51 @@ void input()
     int d; 
     while (true)
     {
-        cout << "Masukkan jumlah elemen pada array: ";
+         cout << "Masukkan jumlah elemen pada array: ";
         cin >> n;
-        if (n > 0 && n <= 20)
+        if (n <= 20)
             break;
         else
+        {
             cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }
     }
+cout << endl;
+cout << "======================" << endl;
+cout << "Masukkan elemen array" << endl;
+cout << "======================" << endl;
 
-}
+for (int i = 0; i < n; i++)
+    {                                           //looping dengan i dimulai dari 0 hingga n-1
+        cout << "Data ke-" << (i + 1) << ": ";  //output ke layar
+        cin >> arr[i];                          //input dari pengguna
+    }                                           
+} 
+
+void bubbleSortArray()
+{                  // prosedur untuk mengurutkan array dengan metode bubble sort
+    int pass = 1;  // step 1
+
+    do
+    {
+        for (int j = 0; j < n - pass; j++)
+        { // step 2
+            if (arr[j] > arr[j + 1])
+            { // step 3
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+            }
+        }
+        pass += 1; // step 4
+
+        cout << "\nPass " << pass - 1 << ": "; //number of pass
+        for (int k = 0; k < n; k++)
+       {
+            cout << arr[k] << " "; //menampilkan data pada number of pass 
+        }
+        cout << endl;
+
+    } while (pass < n); // step 5
+}    
